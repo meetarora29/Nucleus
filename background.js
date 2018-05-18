d54778472ed44760bd581dbc6134c7f8112bf6a0
Meet <meetarora2912@gmail.com>
2018-05-19 00:24:42 +0530

    Correct Async Bug while obtaining stored data

--- background.js
+++ background.js
@@ -8,12 +8,14 @@ var id=0;
 var elapsed=0;
 
 var prev_date=new Date();
-console.log("S:" +prev_date);
+console.log("S: " +prev_date);
 
 var minutes=1000;
 var limit=minutes*(60);
 
 // chrome.storage.sync.clear();
+// chrome.storage.local.clear();
+
 chrome.storage.sync.get(['elapsed', 'date', 'limit'], function (result) {
     console.log("Synced Elapsed is: "+result.elapsed+" Date: "+result.date + " Limit: "+result.limit);
     if(result.elapsed)
@@ -23,31 +25,34 @@ chrome.storage.sync.get(['elapsed', 'date', 'limit'], function (result) {
     if (result.limit)
         limit=result.limit;
     console.log("C: "+prev_date);
+
+	// Taking more recent values if applicable
+	chrome.storage.local.get(['elapsed', 'date', 'limit'], function (result) {
+		console.log("Local Elapsed is: "+result.elapsed+" Date: "+result.date + " Limit: "+result.limit);
+		if (result.date)
+			result.date=new Date(result.date);
+		if(result.date > prev_date) {
+			prev_date=result.date;
+			if (result.limit)
+				limit=result.limit;
+			if (result.elapsed)
+				elapsed=result.elapsed;
+		}
+		console.log("L: "+result.date);
+		console.log("F: " +prev_date);
+	});
+
+	check_new_day();
+	store_values(prev_date);
 });
 
-// Taking more recent values
-chrome.storage.local.get(['elapsed', 'date', 'limit'], function (result) {
-    console.log("Local Elapsed is: "+result.elapsed+" Date: "+result.date + " Limit: "+result.limit);
-    if (result.date)
-        result.date=new Date(result.date);
-    if(result.date > prev_date) {
-        prev_date=result.date;
-        if (result.limit)
-            limit=result.limit;
-        if (result.elapsed)
-            elapsed=result.elapsed;
-    }
-    console.log("L: "+result.date);
-    console.log("F:" +prev_date);
-});
-
-sync_values(prev_date);
 
 function check_new_day() {
     var current_date=new Date();
     current_date.setHours(0, 0, 0, 0);
-    console.log(current_date);
+
     if (current_date>prev_date) {
+        console.log(current_date);
         elapsed=0;
         prev_date=current_date;
     }
@@ -71,8 +76,8 @@ function tick() {
         return;
     elapsed++;
     console.log(elapsed);
-    chrome.storage.local.set({elapsed: elapsed}, function () {
-        console.log("Local value is: "+elapsed);
+    chrome.storage.local.set({'elapsed': elapsed, 'date': new Date().toString()}, function () {
+        console.log("Local value is: "+elapsed + " at " + new Date());
     });
 }
 
@@ -81,8 +86,8 @@ function tock() {
 
     elapsed--;
     console.log(elapsed);
-    chrome.storage.local.set({elapsed: elapsed}, function () {
-        console.log("Local value is: "+elapsed);
+    chrome.storage.local.set({'elapsed': elapsed, 'date': new Date().toString()}, function () {
+        console.log("Local value is: "+elapsed + " at " + new Date());
     });
 }
 
@@ -110,14 +115,25 @@ function timer(url) {
     }
 }
 
-function sync_values(date) {
-    chrome.storage.sync.set({limit: limit, elapsed: elapsed, date: date.toString()}, function () {
+function store_values(date) {
+    chrome.storage.sync.set({'limit': limit, 'elapsed': elapsed, 'date': date.toString()}, function () {
         console.log("Value Synced");
     });
-    chrome.storage.local.set({limit: limit, elapsed: elapsed, date: date.toString()}, function () {
+    chrome.storage.local.set({'limit': limit, 'elapsed': elapsed, 'date': date.toString()}, function () {
         console.log("Local Values Set");
     });
 }
+
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    if (alarm.name === 'saveData') {
+        store_values(new Date());
+    }
+});
+
+chrome.alarms.create('saveData', {
+    periodInMinutes: 1
+});
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function (tab) {
@@ -139,8 +155,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo) {
 
 chrome.windows.onFocusChanged.addListener(function (windowId) {
     if (windowId!==chrome.windows.WINDOW_ID_NONE) {
-        sync_values(new Date());
-
         chrome.tabs.query({active:true, windowId: windowId}, function (result) {
             timer(result[0].url);
             console.log("After window change: "+result[0].url);
@@ -153,7 +167,7 @@ chrome.windows.onFocusChanged.addListener(function (windowId) {
 });
 
 chrome.windows.onRemoved.addListener(function () {
-    sync_values(new Date());
+    store_values(new Date());
 });
 
 chrome.runtime.onInstalled.addListener(function () {
